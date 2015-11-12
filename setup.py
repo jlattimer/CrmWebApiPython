106eb696fcbcf4d4448cd067bf7dc0b203d288eb
jlattimer <jason.lattimer@gmail.com>
2015-11-11 21:16:22 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='CrmWebApiPython',
+    version='1.0.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Jason Lattimer',
+    author_email='',
+    description=''
+)
